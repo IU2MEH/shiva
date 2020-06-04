284534c828d649ead41f393d1ebcc3313b3c44b2
Walter Panella <mulo83@gmail.com>
2020-06-04 12:19:19 +0200

    added timestamp to log file

--- main.cpp
+++ main.cpp
@@ -6,22 +6,23 @@
 void noDebugOutput(QtMsgType type, const QMessageLogContext &context, const QString &msg)
 {
     QByteArray localMsg = msg.toLocal8Bit();
+    QString current_time=QDateTime::currentDateTime().toString("ddMMyyyy-hh:mm:ss");
     switch (type) {
     case QtDebugMsg:
 //        fprintf(stderr, "Debug: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
         break;
     case QtInfoMsg:
 //        fprintf(stderr, "Info: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
-        fprintf(stderr,"Info: %s \n",localMsg.constData());
+        fprintf(stderr,"%s Info: %s \n",current_time.toStdString().c_str(),localMsg.constData());
         break;
     case QtWarningMsg:
 //        fprintf(stderr, "Warning: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
         break;
     case QtCriticalMsg:
-        fprintf(stderr, "Critical: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+        fprintf(stderr, "%s Critical: %s (%s:%u, %s)\n",current_time.toStdString().c_str(), localMsg.constData(), context.file, context.line, context.function);
         break;
     case QtFatalMsg:
-        fprintf(stderr, "Fatal: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+        fprintf(stderr, "%s Fatal: %s (%s:%u, %s)\n",current_time.toStdString().c_str(), localMsg.constData(), context.file, context.line, context.function);
         abort();
     }
 
